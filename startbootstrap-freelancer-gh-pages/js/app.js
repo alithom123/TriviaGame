1a2db832bd97f42a06d39199334db1940a1be7f2
Alex Thompson <alithom123@gmail.com>
2019-01-23 23:20:56 -0600

    fixed quesion answer object constructor

--- startbootstrap-freelancer-gh-pages/js/app.js
+++ startbootstrap-freelancer-gh-pages/js/app.js
@@ -8,15 +8,28 @@ function QA(question, answer) {
     this.answer = answer;
 }
 
-quizbank.push(QA("Golden Girls first aired in 1971.", false));
-quizbank.push(QA("Walter White is the main character in the show 'Better Call Saul.'", false));
-quizbank.push(QA("George Washington's middle name was Walter.", false));
-quizbank.push(QA("Bob Dylan's real name is Robert Zimmerman.", true));
+quizbank.push(new QA("Golden Girls first aired in 1971.", false));
+quizbank.push(new QA("Walter White is the main character in the show 'Better Call Saul.'", false));
+quizbank.push(new QA("George Washington's middle name was Walter.", false));
+quizbank.push(new QA("Bob Dylan's real name is Robert Zimmerman.", true));
+
+var qaTest = new QA("testquestion", true);
+console.log(qaTest);
+console.log(quizbank);
 
 $( document ).ready( function() {
 
 
     // Put questions onto the page.
+
+    for(let i=0; i<numQuestionsPerQuiz; i++) {
+        var questionDiv = $("<div>");
+        var questionParagraph = $("<p>").text(quizbank[i].question);
+        var answerButtons = $("<button>");
+        questionDiv.append(answerButtons);
+        questionDiv.append(questionParagraph);
+        $("#questions").append(questionDiv);
+    }
 
     // If start button is pressed then generate questions, displayQuizScreen(), then start the timer.
 
