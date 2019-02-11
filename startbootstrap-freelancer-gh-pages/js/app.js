f92593250b41de0aa588d21750d43718d433ee3e
Ali Thompson <alithom123@gmail.com>
2019-02-10 23:19:23 -0600

    Questions show. Quiz is graded. Score is shown.

--- startbootstrap-freelancer-gh-pages/js/app.js
+++ startbootstrap-freelancer-gh-pages/js/app.js
@@ -1,50 +1,108 @@
-var quizbank = [];
-var currentQuizQuestions = [];
-var numQuestionsPerQuiz = 3;
-var quizNum = 1;
-var time = 30;
-
-function QA(question, answer) {
-    this.question = question;
-    this.answer = answer;
-}
-
-quizbank.push(new QA("Golden Girls first aired in 1971.", false));
-quizbank.push(new QA("Walter White is the main character in the show 'Better Call Saul.'", false));
-quizbank.push(new QA("George Washington's middle name was Walter.", false));
-quizbank.push(new QA("Bob Dylan's real name is Robert Zimmerman.", true));
-
-var qaTest = new QA("testquestion", true);
-console.log(qaTest);
+// var quizbank = []; Populated in triviaQustions.js
 console.log(quizbank);
+var currentQuizQuestions = [];
+var numQuestionsPerQuiz = 10;
+var quizNum = 1;
+var time = 10;
 
-$( document ).ready( function() {
+$(document).ready(function () {
+    // var startButton = $("<button>").text("Start Quiz").attr("id", "play");
+    // $("#playDiv").append(startButton);
 
 
-    var startButton = $("<button>").text("Start Quiz").attr("id","play");
-    $("#playDiv").append(startButton);
-
-    // Put questions onto the page.
-
-    for(let i=0; i<numQuestionsPerQuiz; i++) {
-        var questionDiv = $("<div>");
-        var questionParagraph = $("<p>").text(quizbank[i].question);
-        var trueButton = $("<button>").text("True");
-        var falseButton = $("<button>").text("False");
-        questionDiv.append(questionParagraph);
-        questionDiv.append(trueButton);
-        questionDiv.append(falseButton);
-        $("#questions").append(questionDiv);
-    }
 
     // If start button is pressed then generate questions, displayQuizScreen(), then start the timer.
     //This is a dynamically added element so you can't use jquery normal like that.
-    $("#play").on("click", function(e) {
-        var interval = setInterval(function(){
-            time--;
-            $("timer").text(time);
-        }, 1000);
+    $("#start").on("click", function (e) {
+
+        // Show Questions.
+        for (let i = 0; i < numQuestionsPerQuiz; i++) {
+            // var questionDiv = $("<div>");
+            let questionsDiv = $("#questionsDiv");
+            // questionsDiv
+            var questionParagraph = $("<p>").text(quizbank[i].question);
+            // var trueButton = $("<button>").text("True");
+            // var falseButton = $("<button>").text("False");
+
+            //      <div class="radio">
+            //          <label><input type="radio" name="optradio" checked>Option 1</label>
+            //      </div>
+            //      <div class="radio">
+            //          <label><input type="radio" name="optradio">Option 2</label>
+            //      </div>
+            questionsDiv.append(questionParagraph);
+            var tfRadioBtns = $('<div class="btn-group btn-group-toggle questionDiv" data-toggle="buttons"><label class="btn btn-secondary active trueFalse" id="q' + i + 'true' + '" data-question-number="' + i + '" data-answer="1" ><input type="radio" name="options" autocomplete="off">True</label><label class="btn btn-secondary trueFalse" id="q' + i + 'false' + '" data-question-number="' + i + '" data-answer="0"><input type="radio" name="options" autocomplete="off">False</label></div>');
+            // var falseRadioBtn = $('<div class="radio"><label><input>FALSE</input></div>');
+
+            //             <div class="btn-group btn-group-toggle" data-toggle="buttons">
+            //   <label class="btn btn-secondary active">
+            //     <input type="radio" name="options" id="option1" autocomplete="off" checked> Active
+            //   </label>
+            //   <label class="btn btn-secondary">
+            //     <input type="radio" name="options" id="option2" autocomplete="off"> Radio
+            //   </label>
+            //   <label class="btn btn-secondary">
+            //     <input type="radio" name="options" id="option3" autocomplete="off"> Radio
+            //   </label>
+            // </div>
+
+            // var falseRadioBtn = $('<input type="radio" name="q' + i + '" />');
+            tfRadioBtns.appendTo(questionsDiv);
+            // falseRadioBtn.appendTo(questionsDiv);
+            // questionDiv.append(trueButton);
+            // questionDiv.append(falseButton);
+            // $("#questionsDiv").append(questionDiv);
+        }
+
+        // console.log($(".trueFalse"));
+
+        // because these are dynamically created buttons.
+        $(".trueFalse").on("click", function (e) {
+            // $("document").on("click", "input", function () { //<-- You will use this a lot probably.
+            console.log(this);
+            console.log($(this));
+            var questionNumber = $(this).attr("data-question-number");
+            var answer = $(this).attr("data-answer");
+            console.log("You answered " + answer + " to question " + questionNumber + ".");
+            if (answer === "1") {
+                answer = true;
+            } else {
+                answer = false;
+            }
+            quizbank[questionNumber].userAnswer = answer;
+
+        });
+
+        var interval = setInterval(function () {
+                console.log("Running setInterval");
+                time--;
+                $("#timer").text(time + " Seconds Remaining.");
+
+                if (time === 0) {
+                    // Get quiz entries.
+                    console.log(quizbank);
+
+                    // Determine score.
+                    let score = 0;
+                    // $.each(quizbank, function () {
+                    for (let i = 0; i < numQuestionsPerQuiz; i++) {
+                        if (quizbank[i].answer === quizbank[i].userAnswer) {
+                            score += 10;
+                        }
+                    };
+
+                    $("#score").append("<h2>Your Score is " + score + "%</h2>");
+                    // Show score.
+                    // Remove timer.
+                    clearInterval(interval);
+
+
+                }
+            },
+            1000);
     });
+
+
 
 });
 
@@ -59,12 +117,12 @@ function displayStartScreen() {
 function generateQuestions(quizNum) {
 
     // Choose three random questions from quizbank.
-    var startingQuestionIndex = (quizNum-1) * numQuestionsPerQuiz;
+    var startingQuestionIndex = (quizNum - 1) * numQuestionsPerQuiz;
     var endingQuestionIndex = startingQuestionIndex + numQuestionsPerQuiz - 1;
-    currentQuizQuestions = quizbank.slice( startingQuestionIndex, endingQuestionIndex);
+    currentQuizQuestions = quizbank.slice(startingQuestionIndex, endingQuestionIndex);
     quizNum++;
     return currentQuizQuestions;
-    
+
 }
 
 function displayQuizScreen() {
@@ -84,8 +142,7 @@ function submitQuiz() {
 function timeoutQuiz() {
 
     // Set answers to none and determine score.
-    
+
     // displayGradeScreen()
 
 }
-
---
+++ startbootstrap-freelancer-gh-pages/js/triviaQuestions.js
@@ -0,0 +1,29 @@
+// Source of questions: https://thepioneerwoman.com/fun-and-learning/true-or-false-trivia-quiz-answers-and-winners/
+
+function QA(question, answer) {
+    this.question = question;
+    this.answer = answer;
+    this.userAnswer = null;
+}
+
+var quizbank = [];
+
+quizbank.push(new QA("The capital of Libya is Benghazi.", false, null));
+quizbank.push(new QA("Albert Einstein was awarded the Nobel Prize in Physics.", true, null));
+quizbank.push(new QA("Baby koalas are called joeys.", true, null));
+quizbank.push(new QA("Gone with the Wind takes place in Savannah, Georgia.", false, null));
+quizbank.push(new QA("Brazil is the only country in the Americas whose official language is Portuguese.", true, null));
+quizbank.push(new QA("The first name of Kramer in Seinfeld is Cosmo.", true, null));
+quizbank.push(new QA("The American Civil War ended in 1776.", false, null));
+quizbank.push(new QA("A right triangle can never be equilateral.", true, null));
+quizbank.push(new QA("The Ford Edsel was named after Henry Ford’ s father.", false, null));
+quizbank.push(new QA("The Dickens novel Oliver Twist begins with the well - known phrase: “It was the best of times, it was the worst of times.”", false, null));
+quizbank.push(new QA("“Paprika” is the Hungarian word for“ pepper.”", true, null));
+quizbank.push(new QA("The Bill of Rights contains 10 amendments to the Constitution.", true, null));
+quizbank.push(new QA("Snow White’ s seven dwarfs all worked as lumberjacks.", false, null));
+quizbank.push(new QA("The name Sylvia Plath was a pseudonym.", false, null));
+quizbank.push(new QA("There are seven red stripes in the United States flag.", true, null));
+quizbank.push(new QA("The favorite food of the Teenage Mutant Ninja Turtles is pizza.", true, null));
+quizbank.push(new QA("Thomas Jefferson was the second president of the United States.", false, null));
+quizbank.push(new QA("No bird can fly backwards.", false, null));
+quizbank.push(new QA("Freddy Kreuger is the villain in the “Friday the 13th” movies.", false, null));
